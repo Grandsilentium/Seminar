abda4a2aef217f1a903bb1ed7267f613e273dad2
Denis <denis_kuznetsov@list.ru>
2022-10-14 01:01:35 +0300

    Case_27 is done

---
+++ Seminar_4/Case_27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int A = number / 10;
+int B = number % 10;
+int sum = A + B;
+
+Console.Write(sum);
+
