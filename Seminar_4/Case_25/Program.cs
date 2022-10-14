9bfc68b66ef39f51028409eea9fc1b9cef98f6b6
Denis <denis_kuznetsov@list.ru>
2022-10-14 13:26:46 +0300

    Case_25 is done

---
+++ Seminar_4/Case_25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+
+void Method1(int number, int degree)
+{
+
+    int numberConstant = number;
+    for(int i = 2; i <= degree; i++)
+        {
+        number = number * numberConstant;
+        }
+    Console.Write($"{numberConstant} ^ {degree} = {number} ");
+
+}
+
+Method1(2,3);
+
+
