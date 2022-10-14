03f25f4e512dfffdf0318f93b01604de6b7fa151
Denis <denis_kuznetsov@list.ru>
2022-10-14 13:22:51 +0300

    Case_27 is done

--- Seminar_4/Case_27/Program.cs
+++ Seminar_4/Case_27/Program.cs
@@ -8,12 +8,15 @@
 
 Console.Clear();
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+void Method2(int number)
+{
+    int sum = 0;
+    while (number>0)
+    {
+        sum = sum + number % 10;
+        number = number/10;
+    }
+    Console.WriteLine(sum);
+}
 
-int A = number / 10;
-int B = number % 10;
-int sum = A + B;
-
-Console.Write(sum);
-
+Method2(9012);
