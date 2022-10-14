0861cf2f10876e11842aad0555ca17c47b10697c
Denis <denis_kuznetsov@list.ru>
2022-10-15 02:54:46 +0300

    Case_34 is done

---
+++ Seminar_5/Case_34/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+Console.Clear();
+
+int size = 4;
+int index = 0;
+int Count = 0;
+
+int [] Array = new int [size];
+
+while (index<size)
+{
+    Array[index] = new Random().Next(100,999);
+    Console.Write($"{Array[index]}  ");
+    if (Array[index] % 2 == 0) Count = Count + 1;
+    index++;
+}
+Console.Write($"-> {Count}");
