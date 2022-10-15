71938c6172f843303978a316235795e7c31396db
Denis <denis_kuznetsov@list.ru>
2022-10-15 16:27:03 +0300

    Case_36 is done

---
+++ Seminar_5/Case_36/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+Console.Clear();
+
+int size = 4;
+int index = 0;
+int sum = 0;
+
+int [] Array = new int [size];
+
+while (index<size)
+{
+    Array[index] = new Random().Next(-10,100);
+    Console.Write($"{Array[index]}  ");
+    if (index % 2 != 0) sum = sum + Array[index];
+    index++;
+}
+Console.Write($"-> {sum}");
