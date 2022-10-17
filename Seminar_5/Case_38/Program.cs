0c698d2eb4ca4b933fc9c8b6f410db87c7594693
Denis <denis_kuznetsov@list.ru>
2022-10-17 17:59:10 +0300

    Case_38 is done. Mistake is fixed

--- Seminar_5/Case_38/Program.cs
+++ Seminar_5/Case_38/Program.cs
@@ -4,27 +4,33 @@
 
 Console.Clear();
 int size = 4;
+
 int [] Array = new int [size];
+for(int i = 0; i < size; i++)   
+{  
+   
+Array[i] = new Random().Next(0,100);
+Console.Write($"{Array[i]} ");
+
+}
+
 int max = Array[0];
 int min = Array[0];
-int index = 0;
-while (index < size)
 
-{
-    Array[index] = new Random().Next(0,100);
-    Console.Write($"{Array[index]} ");
-    if (Array[index] > max) 
-    {
-        max = Array[index];
-    }
-    else 
-    {
-        min = Array[index];
-    }
-    index++;
+
+for(int j = 0; j < size; j++)   
+{  
+   if (Array[j] < min)   
+   {
+       min = Array[j];
+   }
+   if (Array[j] > max) 
+   {
+       max = Array[j];
+   }
 }
-int result = max - min;
 
+int result = max - min;
 Console.Write($"-> {result}");
 
 
